20a428d05af7244494bf4ffb33c4c5eee26b1dde
tgoody <goodmane@ufl.edu>
2021-10-03 17:57:38 -0400

    Getting directories

--- subtitlemover.py
+++ subtitlemover.py
@@ -17,3 +17,23 @@ if not dir_path:
 if not sub_folder_name:
 	print('positional argument "sub_folder_name" not specified (defaulting to "Subs/"')
 	sub_folder_name = 'Subs'
+
+# List all movie folders in "Movies" folder
+for folder in os.listdir(dir_path):
+	# Get contents of each folder
+	items = os.listdir(os.path.join(dir_path, folder))
+	# Check if an mp4 file exists, if not continue
+	movie = [i for i in items if i.endswith('.mp4')]
+	extension = '.mp4'
+	if not movie:
+		movie = [i for i in items if i.endswith('.mkv')]
+		extension = '.mkv'
+		if not movie:
+			print("Media file not found for {}...".format(folder))
+			continue
+	movie_name = movie[0]
+	print('Checking for subtitles in {}...'.format(os.path.join(folder, sub_folder_name)))
+	
+	if('{}.{}.srt'.format(movie_name.rstrip(extension), 'en') in items):
+		print('Found existing subtitle file, skipping...')
+		continue
